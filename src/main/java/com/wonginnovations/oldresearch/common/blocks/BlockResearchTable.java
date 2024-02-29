1f5f0dec484568a09e7e7e5e956617a21daf392e
Wong, Dylan <dylanwong@nevada.unr.edu>
2024-02-29 01:34:15 -0800

    clean up unused

--- src/main/java/com/wonginnovations/oldresearch/common/blocks/BlockResearchTable.java
+++ src/main/java/com/wonginnovations/oldresearch/common/blocks/BlockResearchTable.java
@@ -9,8 +9,6 @@ import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
